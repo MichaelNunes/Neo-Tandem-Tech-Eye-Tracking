71e0b9c814bd952627a61bad925cf925bdb71b0f
CostaxCoffee <u12104592@tuks.co.za>
2015-06-25 12:47:58 +0200

    added main component

---
+++ Final work/Main/NTT Eyetracking/NTT Eyetracking/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NTT_Eyetracking
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Results.Form1 m = new Results.Form1();
+            m.Show();
+        }
+    }
+}
