2002c9264f997833b06cfb3325d97fad8e8eee32
Baruch Molefe <u12260429@tuks.co.za>
2015-06-29 11:39:28 +0200

    Added Server Console & Edited Server Class

---
+++ Final work/Components/Server/Server Class/Server Class/Server.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using TETCSharpClient;
+using TETCSharpClient.Data;
+
+namespace Server_Class
+{
+    public class Server : IGazeListener 
+    {
+        bool recording = false;
+
+        public Server()
+        {
+            Console.WriteLine("Weeee");
+            GazeManager.Instance.Activate(GazeManager.ApiVersion.VERSION_1_0, GazeManager.ClientMode.Push);
+
+            GazeManager.Instance.AddGazeListener(this);
+        }
+
+        public void OnGazeUpdate(GazeData gazeData)
+        {
+            if (!recording) return;
+
+            /*// start or stop tracking lost animation
+            if ((gazeData.State & GazeData.STATE_TRACKING_GAZE) == 0 &&
+                (gazeData.State & GazeData.STATE_TRACKING_PRESENCE) == 0) return;
+
+            // tracking coordinates
+            var x = ActiveScreen.Bounds.X;
+            var y = ActiveScreen.Bounds.Y;
+            var gX = Smooth ? gazeData.SmoothedCoordinates.X : gazeData.RawCoordinates.X;
+            var gY = Smooth ? gazeData.SmoothedCoordinates.Y : gazeData.RawCoordinates.Y;
+            var screenX = (int)Math.Round(x + gX, 0);
+            var screenY = (int)Math.Round(y + gY, 0);
+
+            // return in case of 0,0 
+            if (screenX == 0 && screenY == 0) return;
+
+            NativeMethods.SetCursorPos(screenX, screenY);*/
+        }
+
+        public void close()
+        {
+            GazeManager.Instance.Deactivate();
+        }
+    }
+}
---
+++ Final work/Components/Server/Server Console/Server Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Server_Class;
+using TETCSharpClient;
+
+namespace Server_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server server = new Server();
+            Console.ReadLine();
+        }
+    }
+}
