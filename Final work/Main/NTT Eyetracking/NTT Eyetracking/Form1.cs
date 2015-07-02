6f0556e447e9557c37609a28a6d4186848ee7561
Michael Nunes <u12104592@tuks.co.za>
2015-07-02 11:22:54 +0200

    Main Component
    1)Start new Project
    2)open old project
    3)write to a specified file
    4)create new file
    5)read from settings file

--- Final work/Main/NTT Eyetracking/NTT Eyetracking/Form1.cs
+++ Final work/Main/NTT Eyetracking/NTT Eyetracking/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,7 +21,58 @@ namespace NTT_Eyetracking
         private void button4_Click(object sender, EventArgs e)
         {
             Results.Form1 m = new Results.Form1();
-            m.Show();
+            m.Show();    }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SaveFileDialog Saver = new SaveFileDialog();
+                if (textBox2.Text == "")
+                {
+                    Saver.FileName = "Project";
+                    textBox2.Text = "Project";
+                }
+                else
+                {
+                    Saver.FileName = textBox2.Text;
+
+                }
+                Saver.DefaultExt = ".eye";
+                Saver.Filter = "Eye Project (.eye)|*.eye";
+                Saver.ShowDialog();
+                string FullDirectory = Saver.FileName;
+                textBox1.Text = FullDirectory;
+                string[] array = FullDirectory.Split('\\');
+                string dir = "";
+                for(int i=0;i<array.Length-1;i++)
+                {
+                    dir += array[i] + "\\";
+                }
+                string[] save = new string[4];
+                save[0] = textBox2.Text;
+                save[1] = dir;
+                save[2] = textBox2.Text + ".set";
+                save[3] = "3DModel.set";
+                File.WriteAllLines(Saver.FileName, save);
+            }
+            catch(DirectoryNotFoundException m)
+            {
+                MessageBox.Show(m.Message);
+            }
+            catch(Exception k)
+            {
+                MessageBox.Show(k.Message);
+            }
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            string path = openFileDialog1.FileName;
+            //MessageBox.Show(m);
+            string[] settings = File.ReadAllLines(path);
         }
     }
 }
