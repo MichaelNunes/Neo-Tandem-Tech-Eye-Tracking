af7897b84ec9b14f895b198dabd7b25310ae5c5b
Duran Cole <yipyouguessedit@gmail.com>
2015-07-03 11:27:38 +0200

    Unit Testing: Recording
    1. Added Unit tests to recording class
    2. Minor fix to some properties
    3. All current functionality works

---
+++ Final work/Components/Record/Record_Class/Record_Class/RecordingTesting.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit;
+
+namespace Record_Class
+{
+    [TestFixture]
+    class RecordingTesting
+    {
+        [Test]
+        public void TestRecording()
+        {
+            Record record1 = new Record(@"C:\Users\Public\", "ModelOne");
+            record1.saveToFile();
+
+            record1._recording = true;
+            Assert.AreEqual(true, record1._recording);
+
+            record1._recording = false;
+            Assert.AreEqual(false, record1._recording);
+            
+        }
+
+        [Test]
+        public void TestFileForNoZeroes()
+        {
+            Record record1 = new Record(@"C:\Users\Public\", "ModelOne");
+            record1.saveToFile();
+
+            foreach(string e in record1.arrayData)
+            {
+                Assert.AreNotEqual("0,0", e);
+            }
+
+
+        }
+    }
+}
