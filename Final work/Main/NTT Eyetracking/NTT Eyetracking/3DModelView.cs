29d5ed034d7fc1b10ff6b79ea3ad7cd88aec0749
Michael Nunes <u12104592@tuks.co.za>
2015-07-13 08:35:32 +0200

    Added UI Component and background code
    1)added background code and UI for Video
    2)added background code and UI for 3D

--- Final work/Main/NTT Eyetracking/NTT Eyetracking/3DModelView.cs
+++ Final work/Main/NTT Eyetracking/NTT Eyetracking/3DModelView.cs
@@ -12,9 +12,67 @@ namespace NTT_Eyetracking
 {
     public partial class _3DModelView : Form
     {
+        bool fullscreen = false;
+        string[] imglocation = new string[9];
+        int counter;
         public _3DModelView()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            string folder = folderBrowserDialog1.SelectedPath;
+            //TO DO: add functionality to search folder for images and put them in img array
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            counter = 0;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+            fullscreen = true;
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.Dock = DockStyle.Fill;
+            timer1.Interval = 3000;
+            timer1.Start();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //TO DO:add functionality to open the folder with all the images in them
+        }
+
+        protected override bool ProcessDialogKey(Keys keyData)
+        {
+            if (Form.ModifierKeys == Keys.None && keyData == Keys.Escape)
+            {
+                if (fullscreen == true)
+                {
+                    this.WindowState = FormWindowState.Normal;
+                    this.FormBorderStyle = FormBorderStyle.Sizable;
+                    fullscreen = false;
+                    pictureBox1.Dock = DockStyle.None;
+
+                }
+
+            }
+            return base.ProcessDialogKey(keyData);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (counter == imglocation.Length)
+            {
+                timer1.Stop();
+            }
+            else
+            {
+                pictureBox1.ImageLocation = imglocation[counter];
+                counter++;
+            }
         }
     }
 }
--- Final work/Main/NTT Eyetracking/NTT Eyetracking/VideoView.cs
+++ Final work/Main/NTT Eyetracking/NTT Eyetracking/VideoView.cs
@@ -12,9 +12,68 @@ namespace NTT_Eyetracking
 {
     public partial class VideoView : Form
     {
+        private bool fullscreen = false;
+        bool side = false;
         public VideoView()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            axWindowsMediaPlayer1.URL = openFileDialog1.FileName;
+            axWindowsMediaPlayer1.Ctlcontrols.stop();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+            fullscreen = true;
+            axWindowsMediaPlayer1.Dock = DockStyle.Fill;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+            side = true;
+            axWindowsMediaPlayer1.Dock = DockStyle.Left;
+            axWindowsMediaPlayer2.Dock = DockStyle.Right;
+            axWindowsMediaPlayer1.Width = (this.Width / 2) - 1;
+            axWindowsMediaPlayer2.Width = (this.Width / 2) - 1;
+        }
+        protected override bool ProcessDialogKey(Keys keyData)
+        {
+            if (Form.ModifierKeys == Keys.None && keyData == Keys.Escape)
+            {
+                if (fullscreen == true)
+                {
+                    axWindowsMediaPlayer1.Dock = DockStyle.None;
+                    axWindowsMediaPlayer1.Height = 200;
+                    axWindowsMediaPlayer1.Width = 400;
+                    this.WindowState = FormWindowState.Normal;
+                    this.FormBorderStyle = FormBorderStyle.Sizable;
+                    fullscreen = false;
+
+                }
+                else if (side == true)
+                {
+                    axWindowsMediaPlayer1.Dock = DockStyle.None;
+                    axWindowsMediaPlayer2.Dock = DockStyle.Left;
+                    axWindowsMediaPlayer2.Dock = DockStyle.None;
+                    axWindowsMediaPlayer1.Height = 200;
+                    axWindowsMediaPlayer1.Width = 400;
+                    axWindowsMediaPlayer2.Top += axWindowsMediaPlayer1.Height + 25;
+                    axWindowsMediaPlayer2.Height = 200;
+                    axWindowsMediaPlayer2.Width = 400;
+                    this.WindowState = FormWindowState.Normal;
+                    this.FormBorderStyle = FormBorderStyle.Sizable;
+                    side = false;
+                }
+            }
+            return base.ProcessDialogKey(keyData);
         }
     }
 }
