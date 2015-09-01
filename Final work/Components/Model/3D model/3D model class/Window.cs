796bfe6b64624f875724ae59d0dfbc8b995594fd
Baruch Molefe <u12260429@tuks.co.za>
2015-09-01 10:46:57 +0200

    Added basic fly through mechanisim

--- Final work/Components/Model/3D model/3D model class/Window.cs
+++ Final work/Components/Model/3D model/3D model class/Window.cs
@@ -15,19 +15,29 @@ namespace DisplayModel
         private List<GameObject> objects;
         private string imagePath;
 
+        //Default model view field(s)
         int viewNumber = 0;
         int degrees = 45;
-
         int rotater = 0;
-        
-        GLControl control;
+
+        //Camera feature(s)(make into class)
+        Vector3 cameraPosition = new Vector3(0f, 0f, -2f);
+        float cameraSpeed = 1.0f;
+        float yaw = 0.0f;
+        float pitch = 0.0f;
+
+        //Video recording field(s)
+        Bitmap videoImage;
+        int frameNumber = 0;
+
+        //Screenshot field(s)
+        Bitmap screenShot;
 
         public Window()
             : base(720, 405, new OpenTK.Graphics.GraphicsMode(32, 24, 0, 16))
         {
             shaderData = new Shader();
             objects = new List<GameObject>();
-            control = new GLControl();
         }
 
         public Window(string _imagePath)
@@ -35,7 +45,6 @@ namespace DisplayModel
         {
             shaderData = new Shader();
             objects = new List<GameObject>();
-            control = new GLControl();
             imagePath = _imagePath;
         }
 
@@ -53,6 +62,11 @@ namespace DisplayModel
             {
                 Exit();
             }
+
+            if (e.KeyChar == 'g')
+            {
+                GrabScreenshot();
+            }
         }
 
         protected override void OnLoad(EventArgs e)
@@ -60,9 +74,10 @@ namespace DisplayModel
             base.OnLoad(e);
 
             shaderData.initProgram();
+            videoImage = new Bitmap(this.ClientSize.Width, this.ClientSize.Height);
+            screenShot = new Bitmap(this.ClientSize.Width, this.ClientSize.Height);
 
-
-            Title = "Hello OpenTK!";
+            Title = "3D model viewer";
 
             WindowBorder = WindowBorder.Hidden;
             WindowState = WindowState.Fullscreen;
@@ -74,25 +89,32 @@ namespace DisplayModel
             GL.PointSize(5f);
         }
 
+        /// <summary>
+        /// Updates to data performed irrespective of rendered frame.
+        /// </summary>
+        /// <param name="e"></param>
         protected override void OnUpdateFrame(FrameEventArgs e)
         {
             base.OnUpdateFrame(e);
-        }
 
+            UpdateCamera(e.Time);
+        }
+        
+        /// <summary>
+        /// The current frame to be rendered.
+        /// </summary>
+        /// <param name="e"></param>
         protected override void OnRenderFrame(FrameEventArgs e)
         {
             base.OnRenderFrame(e);
 
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
-            
+
+            defaultView();
             for (int i = 0; i < objects.Count; ++i )
                 shaderData.Draw(objects[i]);
-            
+
             SwapBuffers();
-            //control.PerformContextUpdate();
-            //changeView();
-            //GrabScreenshot();
-            defaultView();
         }
 
         protected override void OnResize(EventArgs e)
@@ -107,40 +129,73 @@ namespace DisplayModel
             GL.LoadMatrix(ref shaderData.ProjectionMatrix);
         }
         /// <summary>
-        /// For debugging purposes.
+        /// This provides the default view with camera control enabled.
         /// </summary>
         public void defaultView()
         {
             for (int i = 0; i < objects.Count; i++)
             {
-                objects[i].bufferData.ModelViewMatrix = Matrix4.Identity * Matrix4.CreateRotationY(MathHelper.DegreesToRadians(rotater++)) * Matrix4.CreateTranslation(0f, 0f, -4f);
+                objects[i].bufferData.ModelViewMatrix = Matrix4.LookAt(cameraPosition, cameraPosition + new Vector3((float)Math.Cos(yaw), pitch, (float)Math.Sin(yaw)),new Vector3(0f, 1f,0f));
             }
         }
 
-        public void changeView()
+        /// <summary>
+        /// This method facilitates the camera movements
+        /// </summary>
+        /// <param name="time">This is the time that the OnUpdateFrame() method takes to make an update so that camera movement can be made to move at a constant speed regardless of the number of updates a second.</param>
+        public void UpdateCamera(double time)
         {
-            if (viewNumber * degrees >= 360)
+            if (Keyboard[OpenTK.Input.Key.W])
             {
-                if (viewNumber > (360/degrees) + 1)
-                {
-                    Exit();
-                }
-                else
-                {
-                    for (int i = 0; i < objects.Count; i++)
-                    {
-                        objects[i].bufferData.ModelViewMatrix = Matrix4.Identity * Matrix4.CreateRotationX(MathHelper.DegreesToRadians(90)) * Matrix4.CreateTranslation(0f, 0f, -4f);
-                    }
-                }
-                viewNumber++;
+                //cameraPosition.Z += cameraSpeed * (float)time;
+                cameraPosition.X += (float)Math.Cos(yaw) * cameraSpeed * (float)time;
+                cameraPosition.Z += (float)Math.Sin(yaw) * cameraSpeed * (float)time;
             }
-            else
+
+            if (Keyboard[OpenTK.Input.Key.S])
             {
-                for (int i = 0; i < objects.Count; i++)
-                {
-                    objects[i].bufferData.ModelViewMatrix = Matrix4.Identity * Matrix4.CreateRotationY(MathHelper.DegreesToRadians(viewNumber * degrees)) * Matrix4.CreateTranslation(0f, 0f, -4f);
-                }
-                viewNumber++;
+                //cameraPosition.Z -= cameraSpeed * (float)time;
+                cameraPosition.X -= (float)Math.Cos(yaw) * cameraSpeed * (float)time;
+                cameraPosition.Z -= (float)Math.Sin(yaw) * cameraSpeed * (float)time;
+            }
+
+            if (Keyboard[OpenTK.Input.Key.A])
+            {
+                //cameraPosition.X += cameraSpeed * (float)time;
+                cameraPosition.X -= (float)Math.Cos(yaw + Math.PI / 2) * cameraSpeed * (float)time;
+                cameraPosition.Z -= (float)Math.Sin(yaw + Math.PI / 2) * cameraSpeed * (float)time;
+            }
+
+            if (Keyboard[OpenTK.Input.Key.D])
+            {
+                //cameraPosition.X -= cameraSpeed * (float)time;
+                cameraPosition.X += (float)Math.Cos(yaw + Math.PI / 2) * cameraSpeed * (float)time;
+                cameraPosition.Z += (float)Math.Sin(yaw + Math.PI / 2) * cameraSpeed * (float)time;
+            }
+
+            //Replace yaw and pitch with mouseYaw and mousePitch
+            if (Keyboard[OpenTK.Input.Key.Left])
+            {
+                //cameraPosition.X += cameraSpeed * (float)time;
+                yaw -= 1.0f * (float)time;
+            }
+
+            if (Keyboard[OpenTK.Input.Key.Right])
+            {
+                //cameraPosition.X -= cameraSpeed * (float)time;
+                yaw += 1.0f * (float)time;
+            }
+
+            if (Keyboard[OpenTK.Input.Key.Up])
+            {
+                //cameraPosition.X += cameraSpeed * (float)time;
+                pitch += 1.0f * (float)time;
+            }
+
+            if (Keyboard[OpenTK.Input.Key.Down])
+            {
+                //cameraPosition.X -= cameraSpeed * (float)time;
+                pitch -= 1.0f * (float)time;
             }
         }
 
@@ -149,15 +204,27 @@ namespace DisplayModel
             if (OpenTK.Graphics.GraphicsContext.CurrentContext == null)
                 throw new OpenTK.Graphics.GraphicsContextMissingException();
 
-            Bitmap bmp = new Bitmap(this.ClientSize.Width, this.ClientSize.Height);
             System.Drawing.Imaging.BitmapData data =
-                bmp.LockBits(this.ClientRectangle, System.Drawing.Imaging.ImageLockMode.WriteOnly, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+                screenShot.LockBits(this.ClientRectangle, System.Drawing.Imaging.ImageLockMode.WriteOnly, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
             GL.ReadPixels(0, 0, this.ClientSize.Width, this.ClientSize.Height, PixelFormat.Bgr, PixelType.UnsignedByte, data.Scan0);
-            bmp.UnlockBits(data);
+            screenShot.UnlockBits(data);
 
-            bmp.RotateFlip(RotateFlipType.RotateNoneFlipY);
-            bmp.Save(imagePath + @"view" + viewNumber + ".jpg");
-            bmp.Dispose();
+            screenShot.RotateFlip(RotateFlipType.RotateNoneFlipY);
+            screenShot.Save(imagePath + @"view" + viewNumber++ + ".bmp");
+        }
+
+        public void RecordVideo()
+        {
+            if (OpenTK.Graphics.GraphicsContext.CurrentContext == null)
+                throw new OpenTK.Graphics.GraphicsContextMissingException();
+
+            System.Drawing.Imaging.BitmapData data =
+                videoImage.LockBits(this.ClientRectangle, System.Drawing.Imaging.ImageLockMode.WriteOnly, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+            GL.ReadPixels(0, 0, this.ClientSize.Width, this.ClientSize.Height, PixelFormat.Bgr, PixelType.UnsignedByte, data.Scan0);
+            videoImage.UnlockBits(data);
+
+            //bmp.RotateFlip(RotateFlipType.RotateNoneFlipY);
+            videoImage.Save(imagePath + @"frame" + (frameNumber++) + ".bmp");
         }
     }
 }
