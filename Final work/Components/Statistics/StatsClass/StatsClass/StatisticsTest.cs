fab1737139352bd91c257d45845fdc5a9d6746dc
Michael Nunes <u12104592@tuks.co.za>
2015-09-08 18:02:52 +0200

    Updated and fix stats

--- Final work/Components/Statistics/StatsClass/StatsClass/StatisticsTest.cs
+++ Final work/Components/Statistics/StatsClass/StatsClass/StatisticsTest.cs
@@ -12,7 +12,7 @@ namespace StatsClass
         [Test]
         public void createPDF()
         {
-            Statistics stats = new Statistics(@"Libraries\Documents\","statsTest",1920,1080,"2D","");
+            Statistics stats = new Statistics(@"Libraries\Documents\", @"Libraries\Documents\", "statsTest", 1920, 1080, "2D", "");
             stats.createPDF();
             
         }
@@ -20,7 +20,7 @@ namespace StatsClass
         [Test]
         public void testHeightandWidthCalculations()
         {
-            Statistics stats = new Statistics(@"Libraries\Documents\", "statsTest", 900, 1200, "2D","");
+            Statistics stats = new Statistics(@"Libraries\Documents\", @"Libraries\Documents\", "statsTest", 900, 1200, "2D", "");
             Assert.AreEqual(400,stats.getGridHeight());
             Assert.AreEqual(300, stats.getGridWidth());     
         }
@@ -28,13 +28,13 @@ namespace StatsClass
         [Test]
         public void testTestforGridpopulation()
         {
-            Statistics stats = new Statistics(@"Libraries\Documents\", "statsTest", 900, 1200, "2D","");
+            Statistics stats = new Statistics(@"Libraries\Documents\", @"Libraries\Documents\", "statsTest", 900, 1200, "2D", "");
 
         }
         [Test]
         public void testcount()
         {
-            Statistics stats = new Statistics(@"Libraries\Documents\", "statsTest", 900, 1200, "2D","");
+            Statistics stats = new Statistics(@"Libraries\Documents\", @"Libraries\Documents\", "statsTest", 900, 1200, "2D", "");
             stats.getgridsCount();
             int c = stats.px.Count;
             float x = stats.px.ElementAt<float>(15);
@@ -44,6 +44,21 @@ namespace StatsClass
             float m =  (float)943.1598;
             Assert.AreEqual(c, 70);
             Assert.AreEqual(x, m);
+
+        }
+        [Test]
+        public void test3D()
+        {
+            Statistics stats = new Statistics(@"Libraries\Documents\", @"Libraries\Documents\", "statsTest", 900, 1200, "3d", "");
+            stats.createPDF3d();
+          //  int c = stats.px.Count;
+            //float x = stats.px.ElementAt<float>(0);
+          //  float y = stats.py.IndexOf(70);
+
+
+          //  float m = (float)943.1598;
+          //  Assert.AreEqual(c, 70);
+          //  Assert.AreEqual(x, m);
 
         }
     }
