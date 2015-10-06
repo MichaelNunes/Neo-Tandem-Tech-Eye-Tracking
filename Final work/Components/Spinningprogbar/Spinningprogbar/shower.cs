79c6b23f830846220c24282477c5e275e5ba0191
Michael Nunes <u12104592@tuks.co.za>
2015-10-06 09:55:18 +0200

    added spinning progress bar

---
+++ Final work/Components/Spinningprogbar/Spinningprogbar/shower.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Spinningprogbar
+{
+    public class shower
+    {
+        internal Form1 m = new Form1();
+        public void showBox()
+        {
+            
+            m.ShowDialog();
+        }
+
+        public void endBox()
+        {
+            m.Close();
+        }
+    }
+}
